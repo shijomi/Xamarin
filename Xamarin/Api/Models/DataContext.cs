0dddb0ff3a88e30074d8b479b37b4b571bdb251b
shijominagashiroq@gmail.com <shijominagashiroq@gmail.com>
2019-12-27 14:33:17 -0400

    creacion de api hasta controlador

---
+++ Xamarin/Api/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace Api.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(): base ("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Api.Models.List> Lists { get; set; }
+    }
+}
---
+++ Xamarin/Api/Models/List.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Models
+{
+    public enum TypeContact
+    { email,
+           
+    }
+    public class List
+    {
+        public int ListID { get; set; }
+        public string Name { get; set; }
+       public TypeContact Type { get; set; }
+    }
+}
